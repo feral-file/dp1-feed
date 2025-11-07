ceeeb9c39869176af64b0d7e1a1964945a3fd988
Hoang Nguyen <hoang@bitmark.com>
2025-11-07 14:58:49 +0700

    Revert "apply ff-dp1-js a custom package of dp1-js"
    This reverts commit 60032a3f9c9de1951c6d46ad2e17a856f664b1e6.

--- routes/playlists.ts
+++ routes/playlists.ts
@@ -1,7 +1,6 @@
 import { Hono, Context } from 'hono';
 import { z } from 'zod';
-import type { Env, PlaylistInput, PlaylistUpdate } from '../types';
-import { Playlist } from 'ff-dp1-js';
+import type { Env, PlaylistInput, PlaylistUpdate, Playlist } from '../types';
 import type { EnvironmentBindings } from '../env/types';
 import type { CreatePlaylistMessage, UpdatePlaylistMessage } from '../queue/interfaces';
 import {
@@ -15,7 +14,7 @@ import { listAllPlaylists, getPlaylistByIdOrSlug, listPlaylistsByChannelId } fro
 import { queueWriteOperation, generateMessageId } from '../queue/processor';
 import { shouldUseAsyncPersistence } from '../rfc7240';
 import { savePlaylist, deletePlaylist } from '../storage';
-import { signDP1Playlist } from 'ff-dp1-js';
+import { signDP1Playlist } from 'dp1-js';
 
 // Create playlist router
 const playlists = new Hono<{ Bindings: EnvironmentBindings; Variables: { env: Env } }>();
